d26bea2acb1028923b11734f0aeac8dbab3f775c
beou <hoaivubk@gmail.com>
2019-01-19 00:35:43 +0700

    update

--- app/src/main/java/mobile/myandroid/MainActivity.java
+++ app/src/main/java/mobile/myandroid/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         List<MainItem> items = new ArrayList<>();
@@ -101,15 +101,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
 
     //-- start new activity
